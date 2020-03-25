49305b58ab996b5df2ed2191bcb7e4815ae6b3e2
Kalev <Kalev@>
2020-03-25 02:17:45 +0200

    good way to manage const-s, however needs a tweek

--- src/board/constants.js
+++ src/board/constants.js
@@ -1,14 +1,14 @@
 export default {
-    
-    
-    
+    HEIGHT : 550,
+    WIDTH : HEIGHT * 0.9,
+    FPS : 30,
     GRID_SIZE : 4,
     CELL : WIDTH / (GRID_SIZE + 2),
     STROKE : CELL / 12,
     DOT : STROKE,
     MARGIN : HEIGHT - (GRID_SIZE + 1) * CELL,
 
-    
+    COLOR_BOARD : "cornsilk",
     COLOR_BORDER : "wheat",
     COLOR_DOT : "sienna",
     COLOR_CPU : "crimson",
